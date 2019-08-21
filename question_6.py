d4d19ce80e1688b73ac451f845be1add3577276c
SanketRanade <54371040+SanketRanade@users.noreply.github.com>
2019-08-21 23:07:35 +0530

    Add files via upload

---
+++ question_6.py
@@ -0,0 +1,47 @@
+import numpy as np
+import math 
+import matplotlib.pyplot as plt
+from coeffs1 import *
+
+
+O = np.array([0,0])
+a =3
+b =3**0.5 
+
+#Generating points on the circle C1
+len = 100
+theta = np.linspace(0,2*np.pi,len)
+x_elips = np.zeros((2,len))
+x_elips[0,:] = a*np.cos(theta)
+x_elips[1,:] = b*np.sin(theta)
+x_elips = (x_elips.T + O).T
+
+
+n1 = np.array([(3/(2**0.5)),(3*(1.5**0.5))])
+n2 = np.array([3/(2**0.5),(1.5**0.5)])
+x_N1 = line_dir_pt(n1,n2,-1.4,0.6)
+
+
+n3 = np.array([(-3/(2**0.5)),(3*(1.5**0.5))])
+n4 = np.array([-3/(2**0.5),(1.5**0.5)])
+x_N2 = line_dir_pt(n3,n4,-1.4,0.6)
+
+#Plotting all lines
+plt.plot(x_N1[0,:],x_N1[1,:],label='$N_1$')
+plt.plot(x_N2[0,:],x_N2[1,:],label='$N_2$')
+
+plt.plot(n2[0], n2[1], 'o')
+plt.text(n2[0] * (1 + 0.1), n2[1] * (1 - 0.1) , 'P')
+plt.plot(n4[0], n4[1], 'o')
+plt.text(n4[0] * (1 - 0.1), n4[1] * (1-0.2) , 'Q')
+
+
+
+plt.plot(x_elips[0,:],x_elips[1,:],label='$ELLIPSE$')
+
+plt.xlabel('$x$');plt.ylabel('$y$')
+plt.grid()
+plt.legend(loc='best')
+plt.axis('equal')
+plt.show()
+
